b39d4d3f07df4c847117c7cf920dedcb44abfaaa
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-11-14 15:48:16 +0000

    Server: dynamically load emails.

--- server.go
+++ server.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/go-martini/martini"
 	"github.com/jelmersnoeck/mondo-email-receiver/gmail"
 )
 
 func main() {
 	g := gmail.NewGmailClient("mondoreceipt@gmail.com")
 
-	email, _ := g.Email("1510670a190c8455")
-	html, _ := email.HTML()
-	fmt.Println(html)
+	m := martini.Classic()
+
+	m.Get("/", func() string {
+		return "Hello Mondo crowd!"
+	})
+
+	m.Get("/emails/:id", func(params martini.Params) string {
+		email, err := g.Email(params["id"])
+
+		if err != nil {
+			return err.Error()
+		}
+
+		html, err := email.HTML()
+
+		if err != nil {
+			return err.Error()
+		}
+
+		return html
+	})
+
+	m.Run()
 }
