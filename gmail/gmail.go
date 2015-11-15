9848899b60529d632cecb3c0d81bc76a9d7e2117
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-11-15 12:05:55 +0000

    Gmail: add documentation.

--- gmail/gmail.go
+++ gmail/gmail.go
@@ -1,3 +1,5 @@
+// Package gmail implements a way to fetch emails from Gmail.
+
 package gmail
 
 import (
@@ -13,41 +15,13 @@ import (
 	"google.golang.org/api/gmail/v1"
 )
 
-// getClient uses a Context and Config to retrieve a Token
-// then generate a Client. It returns the generated Client.
-func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
-	return config.Client(ctx, oauth2Token())
-}
-
-// tokenFromFile retrieves a Token from a given file path.
-// It returns the retrieved Token and any read error encountered.
-func oauth2Token() *oauth2.Token {
-	tm, _ := time.Parse("2006-Jan-02", "2015-Nov-13")
-	return &oauth2.Token{
-		AccessToken:  os.Getenv("GOOGLE_ACCESS_TOKEN"),
-		TokenType:    "Bearer",
-		RefreshToken: os.Getenv("GOOGLE_REFRESH_TOKEN"),
-		Expiry:       tm,
-	}
-}
-
-func getConfig() *oauth2.Config {
-	return &oauth2.Config{
-		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
-		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
-			TokenURL: "https://accounts.google.com/o/oauth2/token",
-		},
-	}
-}
-
 // GmailClient represent a connection with Gmail
 type GmailClient struct {
 	srv   *gmail.Service
 	email string
 }
 
+// NewGmailClient will set up a new GmailClient for the specified email address.
 func NewGmailClient(email string) GmailClient {
 	ctx := context.Background()
 
@@ -61,6 +35,8 @@ func NewGmailClient(email string) GmailClient {
 	return GmailClient{srv, email}
 }
 
+// Email will return an email type that contains the HTML body, subject, sender
+// and related attachments of an email specified by it's ID.
 func (c *GmailClient) Email(id string) (email.Email, error) {
 	call := c.srv.Users.Messages.Get(c.email, id)
 	res, err := call.Format("full").Do()
@@ -84,6 +60,7 @@ func (c *GmailClient) Email(id string) (email.Email, error) {
 	return email, nil
 }
 
+// getMessageBody finds the HTML body of an email.
 func getMessageBody(parts []*gmail.MessagePart) string {
 	for _, part := range parts {
 		if len(part.Parts) > 0 {
@@ -98,6 +75,8 @@ func getMessageBody(parts []*gmail.MessagePart) string {
 	return ""
 }
 
+// getMessageAttachments goes through the message parts for a specific message
+// to find all the image attachments.
 func (c *GmailClient) getMessageAttachments(
 	parts []*gmail.MessagePart,
 	messageId string,
@@ -139,6 +118,7 @@ var imageMimeTypes = []string{
 	"image/gif",
 }
 
+// isImageAttachment validates a mime type to see if it is an image MimeType.
 func isImageAttachment(mime string) bool {
 	for _, tp := range imageMimeTypes {
 		if tp == mime {
@@ -149,14 +129,18 @@ func isImageAttachment(mime string) bool {
 	return false
 }
 
+// getMessageSender goes through the headers to find the From header.
 func getMessageSender(headers []*gmail.MessagePartHeader) string {
 	return getMessageHeader(headers, "From")
 }
 
+// getMessageSubject goes through the headers to find the Subject header.
 func getMessageSubject(headers []*gmail.MessagePartHeader) string {
 	return getMessageHeader(headers, "Subject")
 }
 
+// getMessageHeader goes through a list of headers and returns the header where
+// the name matches the one we want.
 func getMessageHeader(headers []*gmail.MessagePartHeader, wanted string) string {
 	for _, header := range headers {
 		if header.Name == wanted {
@@ -166,4 +150,33 @@ func getMessageHeader(headers []*gmail.MessagePartHeader, wanted string) string 
 	}
 
 	return ""
+}
+
+// getClient uses a Context and Config to retrieve a Token
+// then generate a Client. It returns the generated Client.
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	return config.Client(ctx, oauth2Token())
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func oauth2Token() *oauth2.Token {
+	tm, _ := time.Parse("2006-Jan-02", "2015-Nov-13")
+	return &oauth2.Token{
+		AccessToken:  os.Getenv("GOOGLE_ACCESS_TOKEN"),
+		TokenType:    "Bearer",
+		RefreshToken: os.Getenv("GOOGLE_REFRESH_TOKEN"),
+		Expiry:       tm,
+	}
+}
+
+func getConfig() *oauth2.Config {
+	return &oauth2.Config{
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
+			TokenURL: "https://accounts.google.com/o/oauth2/token",
+		},
+	}
 }
