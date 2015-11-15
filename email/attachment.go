662c68c224a4916d6f91f8a891b250dfdc6d7334
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2015-11-15 12:07:03 +0000

    Attachment: add documentation.

--- email/attachment.go
+++ email/attachment.go
@@ -12,12 +12,15 @@ type Attachment struct {
 	Filename string `json:"filename"`
 }
 
+// HTML will return the value that can be used to put in a HTML attribute.
 func (a Attachment) HTML() string {
 	data, _ := base64.URLEncoding.DecodeString(a.Body)
 	html := base64.StdEncoding.EncodeToString(data)
 	return fmt.Sprintf("data:%s;base64,%s", a.MimeType, html)
 }
 
+// Placeholder will return the placeholder that is used in an email to identify
+// the attachment.
 func (a Attachment) Placeholder() string {
 	return fmt.Sprintf("cid:%s", a.Filename)
 }
